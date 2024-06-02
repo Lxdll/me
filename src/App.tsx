d81c0d807678bfffbff59db92596803981eb8926
luxudong <luxudong@thinredline.com.cn>
2024-06-02 15:11:19 +0800

    chore: init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="w-screen h-screen text-white flex justify-center items-center text-xs">
+        敬请期待...
+      </div>
+    </>
+  );
+}
+
+export default App;
